c4f2ce9fee418223313e176d61faa8dc9482ab2c
Raj Dev Barman <iamrajdev1@gmail.com>
2022-08-14 02:34:15 +0530

    Adding Text Form Field Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -53,5 +53,26 @@ ThemeData getApplicationTheme() {
         bodyText1: getRegularStyle(color: ColorManager.grey)),
 
     //Input decoration theme - Text form field
+    inputDecorationTheme: InputDecorationTheme(
+      contentPadding: const EdgeInsets.all(AppPadding.p8),
+      hintStyle: getRegularStyle(color: ColorManager.grey1),
+      labelStyle: getMediumStyle(color: ColorManager.darkGray),
+      errorStyle: getRegularStyle(color: ColorManager.error),
+      enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
+          borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+      focusedBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+          borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+      errorBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: ColorManager.error, width: AppSize.s1_5),
+          borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+      focusedErrorBorder: OutlineInputBorder(
+          borderSide:
+              BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
+          borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8))),
+    ),
   );
 }
--- lib/presentation/values_manager.dart
+++ lib/presentation/values_manager.dart
@@ -19,10 +19,13 @@ class AppPadding {
 }
 
 class AppSize {
+  static const double s1_5 = 1.5;
   static const double s4 = 4.0;
+  static const double s8 = 8.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;
   static const double s16 = 16.0;
   static const double s18 = 18.0;
   static const double s20 = 20.0;
+  static const double s28 = 28.0;
 }
