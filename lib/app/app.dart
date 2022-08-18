a59ea1ca129cc18cc865a3ad46e9b4116f92e196
Raj Dev Barman <iamrajdev1@gmail.com>
2022-08-18 21:56:10 +0530

    Add Splash Screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../presentation/resources/routes_manager.dart';
 import '../presentation/resources/theme_manager.dart';
 
 class MyApp extends StatefulWidget {
@@ -17,6 +18,9 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      onGenerateRoute: RouteGenerator.getRoute,
+      initialRoute: Routes.splashRoute,
       theme: getApplicationTheme(),
     );
   }
